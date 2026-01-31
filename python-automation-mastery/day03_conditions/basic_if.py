9dec21382d01a123b7d30e508a79f1a8cce763b6
shaheen <shaheenparveen1407@gmail.com>
2026-02-01 02:11:36 +0530

    Day 3 conditions

---
+++ python-automation-mastery/day03_conditions/basic_if.py
@@ -0,0 +1,6 @@
+cpu_usage = 75
+
+if cpu_usage > 85:
+    print("CPU usage is high")
+else:
+    print("CPU usage is normal")
---
+++ python-automation-mastery/day03_conditions/multiple_conditions.py
@@ -0,0 +1,8 @@
+usage = 92
+
+if usage >= 90:
+    print("CRITICAL usage")
+elif usage >= 75:
+    print("WARNING usage")
+else:
+    print("Usage is OK")
